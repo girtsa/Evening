4677e2b1f1c87f680cdbee091b77b89755817f3e
Ģirts Alksnis <girtsa@inbox.lv>
2021-09-15 21:18:50 +0300

    Today15.09.

---
+++ 15.09/Task15.09/Task15.09/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task15._09_
+{
+    
+        public class Person
+        { 
+            public Person(string name)
+        {
+                Name = name;
+                }
+
+        public string Name { get; set; }
+        private int _age;
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                _age = Math.Abs(value);
+            }
+        }
+        public double Money { get; set; }
+        public bool IsMale { get; set; }
+        public char Initial { get; set; }
+        public string GetGreeting()
+        {
+            string greeting = "Hello my name is" + Name + "and I am" + Age + " years old";
+
+            return greeting;
+        }
+
+     }
+}
---
+++ 15.09/Task15.09/Task15.09/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task15._09_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Artis");
+            Person person2 = person;
+
+            // person.Name = "Artis2222";
+            person.Name = "Artis";
+            person.Age = 15;
+            person.IsMale = true;
+            person.Money = 154.58;
+            person.Initial = 'A';
+
+            person2.Age = 26;
+
+            string greeting = person.GetGreeting();
+
+            Console.WriteLine(greeting);
+
+        }
+    }
+}
